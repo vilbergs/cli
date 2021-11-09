d4e049998aa20768d311d5f591ad00188448433d
Vilberg Eliasson <vilbergs@gmail.com>
2021-11-09 13:35:31 +0100

    Allow switching sessions

--- tmux-sessionizer.ts
+++ tmux-sessionizer.ts
@@ -1,7 +1,19 @@
+import { resolve, basename } from 'https://deno.land/std/path/mod.ts'
+
 const HOME = Deno.env.get('HOME')
 
-const p = Deno.run({
-  cmd: ['bash'],
+if (!HOME) {
+  throw Error('no Home')
+}
+
+const dirs = []
+
+for await (const dir of Deno.readDir(HOME)) {
+  dirs.push(resolve(HOME, dir.name))
+}
+
+const fzf = Deno.run({
+  cmd: ['fzf'],
   stdout: 'piped',
   stdin: 'piped',
 })
@@ -9,12 +21,76 @@ const p = Deno.run({
 const encoder = new TextEncoder()
 const decoder = new TextDecoder()
 
-const command = `find ~ ~/sites ~/sites/teachiq ~/sites/vilbergs -mindepth 1 -maxdepth 1 | fzf`
+await fzf.stdin.write(encoder.encode(dirs.join('\n')))
 
-await p.stdin.write(encoder.encode(command))
+await fzf.stdin.close()
 
-await p.stdin.close()
-const output = await p.output()
-p.close()
+const chosenDir = decoder.decode(await fzf.output())
+const sessionName = basename(chosenDir).replaceAll('.', '_').trim()
 
-console.log(decoder.decode(output))
+console.log(sessionName)
+
+const h = await hasSession(sessionName)
+
+console.log(h)
+
+try {
+  if (h) {
+    await switchSession(sessionName)
+  } else {
+    await createSession(sessionName, chosenDir)
+  }
+} catch (e) {
+  console.error(e)
+}
+
+// const p = Deno.run({
+//   cmd: ['tmux', 'switchc -t '],
+//   stdout: 'piped',
+//   stdin: 'piped',
+// })
+
+fzf.close()
+Deno.exit()
+
+async function hasSession(sessionName: string) {
+  const tmux = Deno.run({
+    cmd: ['tmux', 'has-session', '-t', sessionName],
+    stderr: 'piped',
+  })
+  const error = await tmux.stderrOutput()
+
+  tmux.close()
+
+  return error.length === 0
+}
+
+async function createSession(sessionName: string, directory: string) {
+  const tmux = Deno.run({
+    cmd: ['tmux', 'new-session', '-s', sessionName, '-c', directory, '-d'],
+    stderr: 'piped',
+  })
+
+  const error = decoder.decode(await tmux.stderrOutput())
+
+  if (error.length > 0) {
+    throw Error(error)
+  }
+
+  tmux.close()
+}
+
+async function switchSession(sessionName: string) {
+  const tmux = Deno.run({
+    cmd: ['tmux', 'switch-client', '-t', sessionName],
+    stderr: 'piped',
+  })
+
+  const error = decoder.decode(await tmux.stderrOutput())
+
+  if (error.length > 0) {
+    throw Error(error)
+  }
+
+  tmux.close()
+}
