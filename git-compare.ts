e6f5e0f5c63fe04ae2f4d24c64bbaae48d1271e0
Vilberg Eliasson <vilbergs@gmail.com>
2021-11-18 14:22:56 +0100

    Determine default branch

--- git-compare.ts
+++ git-compare.ts
@@ -6,8 +6,13 @@ const repoWithGitExtension = (await getConfigParam('remote.origin.url')).split(
 )[1]
 const repo = repoWithGitExtension.slice(0, repoWithGitExtension.length - 4)
 
+const defaultBranch = await getConfigParam('init.defaultBranch')
+
 const browser = Deno.run({
-  cmd: ['open', `https://github.com/${repo}/compare/master...${branch}`],
+  cmd: [
+    'open',
+    `https://github.com/${repo}/compare/${defaultBranch}...${branch}`,
+  ],
 })
 
 const status = await browser.status()
